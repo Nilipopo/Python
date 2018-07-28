8d055e63b25cc7e807becb09166fcdf0bb6a8b1c
Nilipopo <41773192+Nilipopo@users.noreply.github.com>
2018-07-28 10:01:33 +0800

    Delete ex3.py

--- ex3.py
+++
@@ -1,9 +0,0 @@
-pi=3.1415926
-r=6378 #radium(km)
-print "The circumference of the Earth's equator in meters is:",2*pi*r*1000,"m."
-
-cir=2*pi*r*1000 #circumference
-print "The circumference of the Earth's equator in meters is: %s m."%cir
-
-s=4*pi*r*r #surface area
-print "The Earth's surface area is: %s m^2."%s
