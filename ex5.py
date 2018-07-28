7ff6db8f567227aa97eac7d97b9071dbacecfd04
Nilipopo <41773192+Nilipopo@users.noreply.github.com>
2018-07-28 10:02:17 +0800

    Delete ex5.py

--- ex5.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-pi=np.pi
-r=6378 #radium(km)
-cir=2*pi*r*10**3 #circumference
-print "The circumference of the Earth's equator in meters is:\n\t%sm."%cir
-
-s=4*pi*r*r #surface area
-print "The Earth's surface area is:\n\t%sm^2."%s
